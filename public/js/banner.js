bb4fb2bc3b58a63e9b2601896c705fb32aa37d4c
zhong <123@qq.com>
2019-01-22 21:06:09 +0800

    查询

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,26 +1,75 @@
-$(function(){
+$(function () {
   // alert(1);
 
-  $('#bannerAdd').click(function(){
+  var pageNum = 1;
+  var pageSize = 2;
 
-    $.post('/banner/add',{
-      bannerName: $('#inputEmail3').val(),  
+  //默认调用一次 search
+  search(pageNum, pageSize);
+
+  $('#bannerAdd').click(function () {
+
+    $.post('/banner/add', {
+      bannerName: $('#inputEmail3').val(),
       bannerUrl: $('#inputPassword3').val()
-    },function(res){
+    }, function (res) {
       console.log(res);
-      if(res.code === 0){
+      if (res.code === 0) {
+        layer.msg('添加成功');
         // 成功
-      }else{
+      } else {
         // PS:很多时候,真正的错误信息不回给到用户去看
         console.log(res.msg);
-        alert('网络有误,请稍后重试')
+        layer.msg('网络有误,请稍后重试');
       }
 
       //手动调用关闭的方法
       $('#myModal').modal('hide');
-
+      //手动清空数据框的内容
+      $('#inputEmail3').val('');
+      $('#inputPassword3').val('');
     });
     // alert('发送一个ajax请求');
-    
+
   })
+
+  /**
+   * 查询banner数据的方法
+   * @param {Number} pageNum 当前的页数
+   * @param {Number} pageSize 每页显示的条数
+   */
+  function search(pageNum, pageSize) {
+    $.get('/banner/search', {
+      pageNum: pageNum,
+      pageSize: pageSize
+    }, function (result) {
+      if (result.code === 0) {
+        layer.msg('查询成功');
+
+        for (var i = 0; i < result.data.length; i++) {
+          var item = result.data[i];
+          $('#banner-table tbody').append(
+            `
+              <tr>
+                <td>${item._id}</td>
+                <td>${item.name}</td>
+                <td>
+                  <img  src="${item.imgUrl}" alt="" class="banner-img">
+                </td>
+                <td>
+                  <a href="javascript:;">删除</a>
+                  <a href="javascript:;">修改</a>
+                </td>
+              </tr>
+            `
+          )
+        }
+
+      } else {
+        console.log(result.msg);
+        layer.msg('网络异常,请稍后重试');
+      }
+    })
+  }
+
 })
--- routes/banner.js
+++ routes/banner.js
@@ -1,5 +1,6 @@
 //提供给前端 ajax 调用的接口地址 url
 const express = require('express');
+const async = require('async');
 const BannerModel = require('../models/banner');
 const router = express.Router();
 
@@ -27,4 +28,93 @@ router.post('/add',(req,res) => {
     }
   })
 });
+
+//搜索or查询banner -http://localhost:3000/banner/search
+router.get('/search',(req,res) => {
+  //分页
+  //1.得到前端传递过来的参数
+  let pageNum = Number(req.query.pageNum) || 1;// 当前的页数
+  let pageSize = Number(req.query.pageSize) || 2; //当前显示的条数
+  //let totalSize = 0; //总的条数
+  //采用并行无关联
+  async.parallel([
+    function(cb){
+      BannerModel.find().count().then(num => {
+        // totalSize = num;
+        cb(null,num);
+      }).catch(err => {
+        cb(err);
+      })
+    },
+    function(cb){
+      BannerModel.find().skip(pageNum * pageSize - pageSize).limit(pageSize).then(data => {
+        cb(null,data);
+      }).catch(err => {
+        cb(err);
+      })
+    }
+  ],function(err,result){
+    console.log(result);
+    if(err){
+      res.json({
+        code:-1,
+        msg:err.message
+      })
+    }else{
+      res.json({
+        code: 0,
+        msg:"ok",
+        data:result[1],
+        totalPage:Math.ceil(result[0] / pageSize)//总页数
+        //总条数
+        //totalSize:result[0]
+      })
+    }
+  })
+
+
+  //查看数据的总条数
+  // BannerModel.find().count().then(num => {
+  //   totalSize = num;
+  //   console.log(num);
+  // })
+  // .catch(err => {
+  //   console.log(err.message);
+  // })
+  //分页
+  // BannerModel.find().skip(pageNum * pageSize - pageSize).limit(pageSize).then(result => {
+  //   console.log(result);
+  //   res.json({
+  //     code: 0,
+  //     msg :'ok',
+  //     data:result,
+  //     totalSize:totalSize
+  //   })
+  // })
+  // .catch(err => {
+  //   //出错了
+  //   console.log(err.message);
+  //   res.json({
+  //     code : -1,
+  //     msg : err.message
+  //   })
+  // })
+  // BannerModel.find(function(err,data){
+  //   if(err){
+  //     console.log('查询失败');
+  //     res.json({
+  //       code: -1,
+  //       msg: err.message
+  //     })
+  //   }else{
+  //     console.log('查询成功');
+  //     res.json({
+  //       code: 0,
+  //       msg:'ok',
+  //       data:data
+  //     })
+  //   }
+  // })
+})
+
 module.exports = router;
